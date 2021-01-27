12e897c2f05b5c6b1ca7dac1f9cf10782801c144
iG-SPb <ig_9374318@mail.ru>
2021-01-28 00:18:41 +0300

    step 1

--- TicTacToeButton.py
+++ TicTacToeButton.py
@@ -3,26 +3,28 @@
 
 import sys
 from ctypes import windll
-
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import (QGridLayout, QPushButton, QApplication)
+from PyQt5.QtWidgets import (QGridLayout, QPushButton, QApplication, QProgressBar)
 
 
 class Ex(QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
         super(Ex, self).__init__(*args, **kwargs)
-
+        self.btn_trigger = 0
+        self.step = 0
         windows_size_x = windll.user32.GetSystemMetrics(0)
         windows_size_y = windll.user32.GetSystemMetrics(1)
-        self.layout = QGridLayout(self)
-        self.layout.setSpacing(1)
-        positions = [(i, j) for i in range(10) for j in range(10)]
+        self.grid = QGridLayout(self)
+        self.grid.setSpacing(1)
+        positions = [(i, j) for i in range(5) for j in range(5)]
         for position in positions:
             self.button = QPushButton()
             self.button.setFixedSize(40, 40)
             self.button.clicked.connect(self.buttonClicked)
-            self.layout.addWidget(self.button, *position)
+            self.grid.addWidget(self.button, *position)
+        self.pbar = QProgressBar(self)
+        self.grid.addWidget(self.pbar, 10, 0, 3, 10)
         self.setWindowTitle('TicTacToe')
         self.show()
         w_pos_x = (windows_size_x - self.size().width()) // 2
@@ -32,11 +34,18 @@ class Ex(QtWidgets.QWidget):
 
     def buttonClicked(self):
         button = self.sender()
-        button.setFont(QFont('CeraPro-Bold', 28))
-        button.setStyleSheet('QPushButton {background-color: #A3C1DA; color: red;}')
-        button.setText("X")
-        idx = self.layout.indexOf(button)
-        location = self.layout.getItemPosition(idx)
+        button.setFont(QFont('Time', 28))
+        button.setStyleSheet('QPushButton {background-color: #A3C1DA; color: green;}')
+        self.step = self.step + 1
+        self.pbar.setValue(self.step)
+        if self.btn_trigger == 0:
+            button.setText("X")
+            self.btn_trigger = 1
+        else:
+            button.setText("O")
+            self.btn_trigger = 0
+        idx = self.grid.indexOf(button)
+        location = self.grid.getItemPosition(idx)
         print(location[:2])
 
 
--- TicTacToeGraphics.py
+++ TicTacToeGraphics.py
@@ -4,54 +4,63 @@ import sys
 
 
 class TicTacToe(QGraphicsItem):
-
+    num_lines = 35  # количество строк
+    num_column = 35  # количество столбцов
+    param_x = 600 // num_lines
+    param_y = 600 // num_column
 
     def __init__(self):
         super(TicTacToe, self).__init__()
-        self.board = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
+        self.board = [[-1 for j in range(self.num_lines)] for i in range(self.num_column)]
         self.symbol_AI = 0
         self.symbol_Human = 1
         self.turn = self.symbol_AI
 
     def reset(self):
-        for y in range(3):
-            for x in range(3):
-                self.board[y][x] = -1
+        for x in range(self.num_lines):
+            for y in range(self.num_column):
+                self.board[x][y] = -1
         self.turn = self.symbol_AI
         self.update()
 
     def select(self, x, y):
-        if x < 0 or y < 0 or x >= 3 or y >= 3:
+        if x < 0 or y < 0 or x >= self.num_lines or y >= self.num_column:
             return
-        if self.board[y][x] == -1:
-            self.board[y][x] = self.turn
+        if self.board[x][y] == -1:
+            self.board[x][y] = self.turn
             self.turn = 1 - self.turn
 
-    def paint(self, painter, option, widget):  # рисуем игровое поле
+    def paint(self, painter, option, widget):  # рисуем при каждом нажатии мыши
         painter.setPen(Qt.black)
-        for i in range((600//15), 600, (600//15)):
+        for i in range((600 // self.num_lines), 600, (600 // self.num_column)):
             painter.drawLine(0, i, 600, i)
             painter.drawLine(i, 0, i, 600)
 
-        for y in range(3):
-            for x in range(3):
-                if self.board[y][x] == self.symbol_AI:
+        for y in range(self.num_column):
+            for x in range(self.num_lines):
+                if self.board[x][y] == self.symbol_AI:
                     painter.setPen(Qt.red)
                     painter.drawEllipse(
-                        QPointF(50 + x * 100, 50 + y * 100), 30, 30)
-                elif self.board[y][x] == self.symbol_Human:
+                        QPointF(int(self.param_x / 2) + x * self.param_x,
+                                int(self.param_y / 2) + y * self.param_y),
+                                int(self.param_x / 2) - 2, int(self.param_y / 2) - 2)
+                    print(int(self.param_x / 2) + x * self.param_x,
+                          int(self.param_y / 2) + y * self.param_y)
+                elif self.board[x][y] == self.symbol_Human:
                     painter.setPen(Qt.blue)
-                    painter.drawLine(20 + x * 100, 20 + y * 100,
-                                     80 + x * 100, 80 + y * 100)
-                    painter.drawLine(20 + x * 100, 80 + y * 100,
-                                     80 + x * 100, 20 + y * 100)
+                    painter.drawLine(x * self.param_x, y * self.param_y,
+                                     int(self.param_x) + x * self.param_x, int(self.param_y) + y * self.param_y)
+                    painter.drawLine(int(self.param_x) + x * self.param_x, y * self.param_y,
+                                     x * self.param_x, int(self.param_y) + y * self.param_y)
 
-    def boundingRect(self):
-        return QRectF(0, 0, 600, 600)
+    def boundingRect(self):  # функция определяет внешние границы прямоугольника
+        return QRectF(0, 0, 600, 600)  # возвращает прямоугольник на плоскости
 
     def mousePressEvent(self, event):
         pos = event.pos()
-        self.select(int(pos.x() / 200), int(pos.y() / 200))
+        print(pos.x(), pos.y())
+        print(int(pos.x() / self.param_x), int(pos.y() / self.param_y))
+        self.select(int(pos.x() / self.param_x), int(pos.y() / self.param_y))
         self.update()
         super(TicTacToe, self).mousePressEvent(event)
 
